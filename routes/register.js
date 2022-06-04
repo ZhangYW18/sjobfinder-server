58429f8844f6a41671f1aa399b6964b7d5be5fd1
ZhangYW18 <yiweizh18@gmail.com>
2022-06-04 16:14:56 +0800

    init project

---
+++ routes/register.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST register a user. */
+router.post('/register', function(req, res, next) {
+  // const {username, password} = req.body
+});
+
+module.exports = router;
